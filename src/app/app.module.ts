fe2027321f2b7ccb127fd92417570ff169ad69e9
Darshank81 <dk8129218@gmail.com>
2024-03-09 20:51:26 +0530

    all chages done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ImageComponent } from './image/image.component';
+import { PlaceComponent } from './place/place.component';
+import { VidComponent } from './vid/vid.component';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
@@ -10,28 +16,20 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTooltipModule} from '@angular/material/tooltip';
-
-import { HomeComponent } from './home/home.component';
-import { EventComponent } from './event/event.component';
-import { PlaceComponent } from './place/place.component';
-import { ImageComponent } from './image/image.component';
-import { VidComponent } from './vid/vid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EventComponent,
-    PlaceComponent,
     ImageComponent,
+    PlaceComponent,
     VidComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
